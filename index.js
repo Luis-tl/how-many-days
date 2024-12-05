bdaedc7be2d674670bd7d8f92cec3ede6796e8e3
Luis Tocancipá <luis.tocancipa@bauerxcel.com>
2024-12-05 14:58:52 +0100

    .

---
+++ index.js
@@ -0,0 +1,47 @@
+//calculate the number of days
+function timeCounter(currentDate, importantDay) {
+    let totalTime = currentDate - importantDay;
+    let remainingMilliseconds = totalTime % (1000 * 60 * 60 * 24);
+    let totalYears = currentDate.getFullYear() - importantDay.getFullYear();
+    let totalDays = Math.floor(totalTime / (1000 * 60 * 60 * 24));
+    let totalHours = Math.floor(remainingMilliseconds / (1000 * 60 * 60));
+    let totalMinutes = Math.floor(
+        (remainingMilliseconds % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    let totalSeconds = Math.floor((remainingMilliseconds / 1000) % 60); //<---- Math.floor((remainingMilliseconds % (1000 * 60)) / 1000);
+    return {
+        years: totalYears,
+        days: totalDays,
+        hours: totalHours,
+        minutes: totalMinutes,
+        seconds: totalSeconds,
+    };
+}
+
+function timeWriter() {
+    //Days to calculate
+    const currentDate = new Date();
+    const importantDay = new Date("June 19, 2023 11:50:00");
+
+    // call the funtion to calculate the time and pass the two days
+    const result = timeCounter(currentDate, importantDay);
+
+    // Insert the days and hours into the html
+    let containerDays = document.getElementById("dayNumber");
+    containerDays.textContent = result.days;
+
+    let containerHours = document.getElementById("hourNumber");
+    containerHours.textContent = result.hours;
+
+    let containerMinutes = document.getElementById("hourMinutes");
+    containerMinutes.textContent = result.minutes;
+
+    let containerSeconds = document.getElementById("hourSeconds");
+    containerSeconds.textContent = result.seconds;
+}
+
+setInterval(timeWriter, 1000);
+
+timeWriter();
+
+//https://codepen.io/nitnelav/pen/povrOME
